14ae30edebf6da88865ed3ce0f3b0b2e1f8d0288
Julia <email96123@gmail.com>
2023-08-28 17:11:02 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// popups
+function createPopup(imageSrc) {
+  const popup = document.createElement("div");
+  popup.classList.add("popup");
+
+  const image = document.createElement("img");
+  image.src = imageSrc;
+  popup.appendChild(image);
+
+  positionPopupRandomly(popup);
+
+  return popup;
+}
+
+function positionPopupRandomly(popup) {
+  const randomX = Math.random() * (window.innerWidth - popup.offsetWidth);
+  const randomY = Math.random() * (window.innerHeight - popup.offsetHeight);
+
+  popup.style.left = `${randomX}px`;
+  popup.style.top = `${randomY}px`;
+}
+
+const gifs = [
+  "possum1.gif",
+  "possum4.gif",
+  "possum.gif",
+  "possum2.gif",
+  "possum3.gif",
+];
+
+let count = 0;
+
+setInterval(() => {
+  count = (count + 1) % gifs.length;
+  const popup = createPopup(gifs[count]);
+  document.body.appendChild(popup);
+}, 1000);
+
+// text animation
+const spansSlow = document.querySelectorAll(".span");
+
+window.addEventListener("scroll", handleScroll);
+
+function handleScroll() {
+  const scrollPosition = window.scrollY;
+
+  spansSlow.forEach((span) => {
+    const translation = scrollPosition / 2;
+    span.style.transform = `translateX(${translation}px)`;
+  });
+}
